460f72c56acd9bb20bfe38bfee9ad2effcb832e2
wassimhamdi <hamdi.wassim91@gmail.com>
2014-12-09 22:46:26 +0100

    first commit

---
+++ wass hoss.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class wasshoss
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(400, 145)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(265, 145)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Valider"
+        Me.Button1.UseVisualStyleBackColor = true
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Label1.Font = New System.Drawing.Font("Palatino Linotype", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(162,Byte))
+        Me.Label1.Location = New System.Drawing.Point(47, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 20)
+        Me.Label1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(106, 147)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Numéro de la matière"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Label3.Font = New System.Drawing.Font("Palatino Linotype", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(162,Byte))
+        Me.Label3.Location = New System.Drawing.Point(28, 27)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 20)
+        Me.Label3.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(265, 145)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Valider"
+        Me.Button2.UseVisualStyleBackColor = true
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(400, 144)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = true
+        Me.Label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Label4.Font = New System.Drawing.Font("Palatino Linotype", 11.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(162,Byte))
+        Me.Label4.Location = New System.Drawing.Point(28, 42)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 20)
+        Me.Label4.TabIndex = 7
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(51, 275)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Suivant"
+        Me.Button3.UseVisualStyleBackColor = true
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = true
+        Me.RadioButton1.Location = New System.Drawing.Point(51, 241)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(32, 17)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = true
+        Me.RadioButton1.Text = "A"
+        Me.RadioButton1.UseVisualStyleBackColor = true
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = true
+        Me.RadioButton2.Location = New System.Drawing.Point(93, 241)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(32, 17)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = true
+        Me.RadioButton2.Text = "B"
+        Me.RadioButton2.UseVisualStyleBackColor = true
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = true
+        Me.RadioButton3.Location = New System.Drawing.Point(131, 241)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(32, 17)
+        Me.RadioButton3.TabIndex = 11
+        Me.RadioButton3.TabStop = true
+        Me.RadioButton3.Text = "C"
+        Me.RadioButton3.UseVisualStyleBackColor = true
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = true
+        Me.RadioButton4.Location = New System.Drawing.Point(180, 241)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(33, 17)
+        Me.RadioButton4.TabIndex = 12
+        Me.RadioButton4.TabStop = true
+        Me.RadioButton4.Text = "D"
+        Me.RadioButton4.UseVisualStyleBackColor = true
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(707, 39)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(85, 23)
+        Me.Button4.TabIndex = 13
+        Me.Button4.Text = "recommencer"
+        Me.Button4.UseVisualStyleBackColor = true
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(32, 304)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(477, 23)
+        Me.ProgressBar1.TabIndex = 14
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = true
+        Me.Label5.Font = New System.Drawing.Font("Rockwell", 12!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic),System.Drawing.FontStyle))
+        Me.Label5.Location = New System.Drawing.Point(713, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 20)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Label5"
+        '
+        'Timer2
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.qcm.My.Resources.Resources.ee1
+        Me.PictureBox1.Image = Global.qcm.My.Resources.Resources.ee1
+        Me.PictureBox1.Location = New System.Drawing.Point(32, 174)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(770, 169)
+        Me.PictureBox1.TabIndex = 16
+        Me.PictureBox1.TabStop = false
+        '
+        'wasshoss
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientInactiveCaption
+        Me.ClientSize = New System.Drawing.Size(814, 335)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "wasshoss"
+        Me.Text = "QCM"
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ wass hoss.vb
@@ -0,0 +1,190 @@
+﻿Public Class wasshoss
+
+    Public service As New ser.mywsClient()
+    Public r3(10) As String
+    Public r4(10) As String
+    Public r5 As String
+    Public correct As Integer
+    Public fal As Integer
+    Public j As Integer
+    Public rep As String
+    Public Timer1 As New Timer
+    
+
+    Private Sub wasshoss_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        r5 = ""
+        Timer2.Start()
+        PictureBox1.Visible = True
+        ProgressBar1.Value = 0
+        ProgressBar1.Visible = False
+        ProgressBar1.Maximum = 100
+        Label1.Text = ""
+        Label3.Text = ""
+        Label4.Text = ""
+        correct = 0
+        fal = 0
+        Label4.Visible = False
+        Button4.Visible = False
+        Label1.Visible = True
+        Label2.Visible = True
+        Button2.Visible = False
+        Button1.Visible = True
+        Button3.Visible = False
+        TextBox2.Visible = False
+        TextBox1.Visible = True
+        RadioButton1.Visible = False
+        RadioButton2.Visible = False
+        RadioButton3.Visible = False
+        RadioButton4.Visible = False
+        Dim r(10) As String
+        r = service.matiere
+        Dim i As Integer
+        For i = 0 To 100
+            If r(i).CompareTo("") = 0 Then
+                Exit For
+            Else
+                Label1.Text = Label1.Text + r(i) + Chr(10)
+
+            End If
+        Next
+
+
+    End Sub
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button2.Visible = True
+        TextBox2.Visible = True
+        Label1.Visible = False
+        Label3.Visible = True
+        TextBox1.Visible = False
+        Button1.Visible = False
+        Dim r1(10) As String
+        r1 = service.test(Convert.ToInt32(TextBox1.Text))
+        Dim i As Integer
+        For i = 0 To 100
+            If r1(i).CompareTo("") = 0 Then
+                Exit For
+            Else
+                Label3.Text = Label3.Text + r1(i) + Chr(10)
+                Label2.Text = ("Numéro du chapitre")
+
+            End If
+        Next
+
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label4.Visible = True
+        ProgressBar1.Visible = True
+        PictureBox1.Visible = False
+        j = 0
+        r3 = service.question(Convert.ToInt32(TextBox2.Text))
+        r4 = service.rep(Convert.ToInt32(TextBox2.Text))
+        Label4.Text = Convert.ToString(j + 1) + "- " + r3(j) + Chr(10)
+        j = j + 1
+        RadioButton1.Visible = True
+        RadioButton2.Visible = True
+        RadioButton3.Visible = True
+        RadioButton4.Visible = True
+        Button2.Visible = False
+        Button3.Visible = True
+        Label2.Visible = False
+        Label3.Visible = False
+        TextBox2.Visible = False
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim str As String
+        Dim i As Integer
+        If RadioButton1.Checked = False And RadioButton2.Checked = False And RadioButton3.Checked = False And RadioButton4.Checked = False Then
+            MsgBox("Vous devez choisir une réponse !")
+        Else
+            If RadioButton1.Checked = True Then
+                rep = "1"
+            ElseIf RadioButton2.Checked = True Then
+                rep = "2"
+            ElseIf RadioButton3.Checked = True Then
+                rep = "3"
+            ElseIf RadioButton4.Checked = True Then
+                rep = "4"
+            End If
+            str = r4(j - 1)
+            ProgressBar1.Value = (j * 100) / 9
+            PictureBox1.Visible = False
+
+
+            If rep.CompareTo(Convert.ToString(str.ElementAt(0))) = 0 Then
+                r5 = r5 + Convert.ToString(j) + "- bonne réponse " + Chr(10) + Chr(10)
+                correct = correct + 1
+            Else
+                r5 = r5 + Convert.ToString(j) + "- mauvaise réponse, la bonne réponse est " + str + Chr(10) + Chr(10)
+                fal = fal + 1
+            End If
+            If r3(j).CompareTo("") = 0 Then
+                ProgressBar1.Visible = False
+                Button4.Visible = True
+                Button3.Visible = False
+                RadioButton1.Visible = False
+                RadioButton2.Visible = False
+                RadioButton3.Visible = False
+                RadioButton4.Visible = False
+                Dim pourcentage As Integer
+                pourcentage = (correct / (correct + fal)) * 100
+                If pourcentage > 75 Then
+                    MsgBox("Score : " + Convert.ToString(pourcentage) + "%")
+                Else
+                    MsgBox("Score : " + Convert.ToString(pourcentage) + "%")
+                End If
+                Label4.Text = Chr(10) + Chr(10) + r5
+
+            Else
+                Label4.Text = Convert.ToString(j + 1) + "- " + r3(j) + Chr(10)
+                j = j + 1
+            End If
+            RadioButton1.Checked = False
+            RadioButton2.Checked = False
+            RadioButton3.Checked = False
+            RadioButton4.Checked = False
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.wasshoss_Load(sender, e)
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Label5.Text = TimeOfDay
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
